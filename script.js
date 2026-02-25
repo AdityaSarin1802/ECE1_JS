798df2da487789b77c994b3cb32b352a2f1a7bd6
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-02-25 09:50:12 +0530

    variable data type

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello world");
+var num;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Hello";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=10;
+console.log(num);
+console.log(typeof(num));
