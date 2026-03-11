f45eabdb76871bda8e7e14b81df23c5b026703bb
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-03-11 10:15:09 +0530

    functions in javascript

---
+++ function.js
@@ -0,0 +1,13 @@
+// function myFunction(){
+//     alert("Hello World");
+// }
+
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" years old ");
+// }
+
+//arrow func
+var add3=(a,b)=>{
+    console.log(a+b);
+};
+add3(110,113);
