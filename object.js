f16e4ad5ba54d096055d83d7d6f36290548582b7
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-03-11 10:28:27 +0530

    object (reference text)

---
+++ object.js
@@ -0,0 +1,17 @@
+// var person={
+//     name:'John Max', age:30, isMarried: true,
+//     address:{
+//         street: 'vijaya nagar', flatNO: 201
+//     }
+// };
+// console.log(person);
+
+var person2= new Object();
+person2.name='John Max';
+person2.age=30;
+person2.isMarried= true;
+person2.address={};
+person2.address.street= 'vijaya nagar';
+person2['address']['flatNO']= 201;
+
+console.log(person2);
