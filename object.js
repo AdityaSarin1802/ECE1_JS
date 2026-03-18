9580ab289e056dde0235847517d49d5be6e95f5c
AdityaSarin1802 <sarin.aditya18@gmail.com>
2026-03-18 09:08:31 +0530

    object in javascript

--- object.js
+++ object.js
@@ -6,12 +6,16 @@
 // };
 // console.log(person);
 
-var person2= new Object();
-person2.name='John Max';
-person2.age=30;
-person2.isMarried= true;
-person2.address={};
-person2.address.street= 'vijaya nagar';
-person2['address']['flatNO']= 201;
+// var person2= new Object();
+// person2.name='John Max';
+// person2.age=30;
+// person2.isMarried= true;
+// person2.address={};
+// person2.address.street= 'vijaya nagar';
+// person2['address']['flatNO']= 201;
 
-console.log(person2);
+// console.log(person2);
+
+var myinfo= new Array();
+var myinfo1=Array();
+console.log(myinfo,myinfo1);
